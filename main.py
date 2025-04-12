29f70e093be9b3ed4b046aa5ed83c1df5b1028cf
unknown <Student@412-05.kpc408.ru>
2025-04-12 14:00:00 +0300

    проверка дискриминанта

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ a = float(input("Введите коэффициент а:"))
 b = float(input("Введите коэффициент b:"))
 c = float(input("Введите коэффициент c:"))
 D = b**2 - 4 * a * c
+
+if D > 0:
+    pass
+
+elif D == 0:
+    pass
+
+else:
+    print("Нет корней")
