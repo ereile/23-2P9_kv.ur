28a11dd40a822f4d3045dc2db9324c8c005dc84d
M2K-5F <ivan.semjonow15@gmil.com>
2025-04-26 11:29:57 +0300

    tkinter

--- desktop.py
+++ desktop.py
@@ -1,7 +1,12 @@
+"""TKINTER"""
+
 import tkinter as tk
+from main import kv_ur
 
 
 class App:
+    """_summary_
+    """
     def __init__(self, master: tk.Tk) -> None:
         self.master = master
         self.master.title('Решение квадратного уравнения')
@@ -10,6 +15,32 @@ class App:
         self.input_a = tk.Entry(self.master)
         self.input_b = tk.Entry(self.master)
         self.input_c = tk.Entry(self.master)
+        self.label = tk.Label(self.master, text='')
 
-        self.button = tk.Button(self.master, text='Вычислить')
+        self.input_a.pack()
+        self.input_b.pack()
+        self.input_c.pack()
 
+        self.button = tk.Button(self.master,
+                                text='Вычислить',
+                                command=self.solution
+                                )
+
+        self.button.pack()
+        self.label.pack()
+
+    def solution(self):
+        """_summary_"""
+
+        try:
+            a = int(self.input_a.get())
+            b = int(self.input_b.get())
+            c = int(self.input_c.get())
+            self.label.config(text=str(kv_ur(a, b ,c)))
+
+        except ValueError:
+            pass
+
+root = tk.Tk()
+app = App(root)
+root.mainloop()
