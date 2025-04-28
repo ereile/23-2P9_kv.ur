f8e1b401e1bf372eaadb64a4ef05c4c0a8c85222
M2K-5F <ivan.semjonow15@gmail.com>
2025-04-28 16:48:50 +0300

    Changes tkinter

--- desktop.py
+++ desktop.py
@@ -1,6 +1,7 @@
 """TKINTER"""
 
 import tkinter as tk
+from typing import List, Tuple
 from main import kv_ur
 
 
@@ -10,32 +11,29 @@ class App(tk.Tk):
         super().__init__()
         self.title('Решение квадратного уравнения')
         self.geometry('500x250')
-
-        self.input_a = tk.Entry(self)
-        self.input_b = tk.Entry(self)
-        self.input_c = tk.Entry(self)
+        self.inputs:  List[Tuple[tk.Entry, tk.Label]] = []
         self.solution_label = tk.Label(self.master, text='')
-        self.callback_a_label = tk.Label(self)
-        self.callback_b_label = tk.Label(self)
-        self.callback_c_label = tk.Label(self)
+
+        for _ in range(3):
+            label = tk.Label(self)
+            input_ = tk.Entry(self)
+            label.pack()
+            input_.pack()
+            self.inputs.append((input_, label))
+
         self.button = tk.Button(self.master,
                                 text='Вычислить',
                                 command=self.solution
                                 )
-        self.inputs = [
-            (self.input_a, self.callback_a_label),
-            (self.input_b, self.callback_b_label),
-            (self.input_c, self.callback_c_label)
-            ]
 
-        self.callback_a_label.pack()
-        self.input_a.pack()
-        self.callback_b_label.pack()
-        self.input_b.pack()
-        self.callback_c_label.pack()
-        self.input_c.pack()
         self.button.pack()
         self.solution_label.pack()
+
+    def set_label(self, string: str):
+        """Set labels"""
+
+        for (_, lbl) in self.inputs:
+            lbl.config(text=string)
 
     def input_callback(self, label: tk.Label):
         """Except callback"""
@@ -44,9 +42,10 @@ class App(tk.Tk):
     def solution(self):
         """solution"""
 
+        self.set_label('')
         params = []
         flag = True
-        for [input_el, label] in self.inputs:
+        for (input_el, label) in self.inputs:
             try:
                 params.append(int(input_el.get()))
 
@@ -56,8 +55,7 @@ class App(tk.Tk):
                 break
 
         if flag:
-            for lst in self.inputs:
-                lst[1].config(text='')
+            self.set_label('')
 
             self.solution_label.config(text=str(
                                        kv_ur(
