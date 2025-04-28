3ce3ed81af4ac4f2cdd4e739e0fb27a3e6469882
priyatei <priyatel_official@mail.ru>
2025-04-28 16:57:12 +0300

    api для решение уравнения

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from main import kv_ur
 app = FastAPI()
 
 
-@app.get("/get_answer/")
+@app.get("/kvur/")
 async def get_answer(a: float, b: float, c: float):
     """Получения ответа"""
     output = kv_ur(a, b, c)
