bc9d4c2455a99ee9df6bce9de399ce60b58e76b2
M2K-5F <ivan.semjonow15@gmail.com>
2025-04-28 17:42:04 +0300

    fix async

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 
 @app.get("/kvur/")
-def get_answer(a: float, b: float, c: float):
+async def get_answer(a: float, b: float, c: float):
     """Получения ответа"""
     output = kv_ur(a, b, c)
     return output
