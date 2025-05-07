203dd7836f1487cb02dcc622393260b554aac509
M2K-5F <ivan.semjonow15@gmail.com>
2025-05-07 10:09:20 +0300

    fix pylint

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,16 +10,16 @@ URL = "/kvur/"
 class TestsKvUrAPI(unittest.TestCase):
     """Class for unittests API"""
 
-    def test_three_is_zero(self):
+    def test_api_three_is_zero(self):
         """Прямая параллельна оси"""
 
-        a = 0
-        b = 0
-        c = 4
+        a_in = 0
+        b_in = 0
+        c_in = 4
         answer = "Нет решения"
         string = "Линейное уравнение параллельное оси Ох"
         output = requests.get(
-            f"http://{HOST}{URL}?a={a}&b={b}&c={c}",
+            f"http://{HOST}{URL}?a={a_in}&b={b_in}&c={c_in}",
             timeout=1000
             ).json()
         count = 2
