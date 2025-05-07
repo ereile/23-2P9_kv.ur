acdd2069e5bf5078c7b46ac87e9ef3bb9ec4f6ea
M2K-5F <ivan.semjonow15@gmail.com>
2025-05-07 09:40:09 +0300

    fix flake

--- tests/test_API.py
+++ tests/test_API.py
@@ -11,18 +11,18 @@ class TestsKvUrAPI(unittest.TestCase):
     """Class for unittests API"""
 
     def test_three_is_zero(self):
-            """Прямая параллельна оси"""
+        """Прямая параллельна оси"""
 
-            a = 0
-            b = 0
-            c = 4
-            answer = "Нет решения"
-            string = "Линейное уравнение параллельное оси Ох"
-            output = requests.get(
-                f"http://{HOST}{URL}?a={a}&b={b}&c={c}"
-                ).json()
-            count = 2
+        a = 0
+        b = 0
+        c = 4
+        answer = "Нет решения"
+        string = "Линейное уравнение параллельное оси Ох"
+        output = requests.get(
+            f"http://{HOST}{URL}?a={a}&b={b}&c={c}"
+            ).json()
+        count = 2
 
-            self.assertEqual(len(output), count, "Некорректное число элементов")
-            self.assertEqual(output[0], string, "Строка некорреткна")
-            self.assertEqual(output[1], answer, "Ответ некорректен")
+        self.assertEqual(len(output), count, "Некорректное число элементов")
+        self.assertEqual(output[0], string, "Строка некорреткна")
+        self.assertEqual(output[1], answer, "Ответ некорректен")
