3ae76d7a6491fd12a9b2fb9a29120f5e703a1c6e
olesya <ol.pavlova555@gmail.com>
2025-05-07 09:31:49 +0300

    Вывод страницы

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 """api для решение уравнения"""
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from main import kv_ur
+
 app = FastAPI()
 
 
@@ -9,3 +11,10 @@ async def get_answer(a: float, b: float, c: float):
     """Получения ответа"""
     output = kv_ur(a, b, c)
     return output
+
+
+@app.get("/", response_class=HTMLResponse)
+async def page_output():
+    """Вывод страницы"""
+    with open("index.html", "r", encoding="utf-8") as f:
+        return f.read()
